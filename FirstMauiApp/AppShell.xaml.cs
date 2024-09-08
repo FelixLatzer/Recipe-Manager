324e9b98575cf72ba9afd612e03456dd480de7ef
Felix Latzer <latzer.felix@gmail.com>
2024-09-08 14:33:45 +0200

    Adds project

---
+++ FirstMauiApp/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using FirstMauiApp.Views;
+
+namespace FirstMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(AddRecipeView), typeof(AddRecipeView));
+        }
+    }
+}
---
+++ FirstMauiApp/Views/AddRecipeView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FirstMauiApp.Views;
+
+public partial class AddRecipeView : ContentPage
+{
+	public AddRecipeView()
+	{
+		InitializeComponent();
+	}
+}
